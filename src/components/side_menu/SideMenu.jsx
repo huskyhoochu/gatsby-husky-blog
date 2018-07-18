57aeca4fe7b9f228bc401fa8832742fa2364e220
huskyhoochu <dfg1499@gmail.com>
2018-07-18 16:49:05 +0900

    side menu 컴포넌트 생성

---
+++ src/components/side_menu/SideMenu.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+// CSS
+import './SideMenu.css';
+
+export default () => (
+  <aside className="menu">
+    <div>
+      <ul className="menu-list">
+        <li>
+          <Link to="/">Latest</Link>
+        </li>
+      </ul>
+      <p className="menu-label">Programming</p>
+      <ul className="menu-list">
+        <li>
+          <Link to="/">Frontend</Link>
+        </li>
+        <li>
+          <Link to="/">Backend</Link>
+        </li>
+      </ul>
+      <p className="menu-label">Literature</p>
+      <ul className="menu-list">
+        <li>
+          <Link to="/">Essay</Link>
+        </li>
+        <li>
+          <Link to="/">Review</Link>
+        </li>
+      </ul>
+      <p className="menu-label">About</p>
+      <ul className="menu-list">
+        <li>
+          <Link to="/">About Me</Link>
+        </li>
+      </ul>
+    </div>
+  </aside>
+);
