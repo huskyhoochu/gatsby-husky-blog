74f4de33f4f2dd286101deadd3a4ec428acfa95e
huskyhoochu <dfg1499@gmail.com>
2018-07-18 16:49:43 +0900

    전체 레이아웃 통합하는 레이아웃 컴포넌트 생성

---
+++ src/components/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Components
+import SideMenu from './side_menu/SideMenu';
+
+export default () => (
+  <div id="canvas" className="columns is-desktop is-gapless">
+    <SideMenu />
+  </div>
+);
