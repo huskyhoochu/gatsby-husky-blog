fbde30ee9bb9bc021bd26e834605fbfe9ff7721f
huskyhoochu <dfg1499@gmail.com>
2018-07-18 17:45:49 +0900

    좌측 섹션 컴포넌트 생성

---
+++ src/components/left_section/LeftSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// CSS
+import './LeftSection.css';
+
+// Components
+import Header from '../header/Header';
+
+export default () => (
+  <div id="left" className="column">
+    <Header />
+  </div>
+);
