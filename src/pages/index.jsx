99e149328a588b4e1761bf7e04045a14411fee47
huskyhoochu <dfg1499@gmail.com>
2018-07-18 16:48:16 +0900

    lint & prettier

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,8 +7,8 @@ import 'bulma/css/bulma.min.css';
 import Layout from '../components/Layout';
 
 export default () => (
-      <div>
-        <h1 className="title">Welcome husky</h1>
-        <Layout/>
-      </div>
+  <div>
+    <h1 className="title">Welcome husky</h1>
+    <Layout />
+  </div>
 );
