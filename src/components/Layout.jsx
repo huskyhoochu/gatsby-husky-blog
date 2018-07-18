d211bcf2c798c20ad2ff02ae67ad5ce29e90d3d8
huskyhoochu <dfg1499@gmail.com>
2018-07-18 17:37:42 +0900

    left section 컴포넌트 삽입

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 // Components
 import SideMenu from './side_menu/SideMenu';
+import LeftSection from './left_section/LeftSection';
 
 export default () => (
   <div id="canvas" className="columns is-desktop is-gapless">
     <SideMenu />
+    <LeftSection />
   </div>
 );
