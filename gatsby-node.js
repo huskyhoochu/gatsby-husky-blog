1bbece90317f01984c65d7cc0f1d04e9f2f0d693
huskyhoochu <dfg1499@gmail.com>
2018-07-19 18:37:50 +0900

    gatsby-node 생성

---
+++ gatsby-node.js
@@ -0,0 +1,64 @@
+const _ = require('lodash');
+const Promise = require('bluebird');
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  return new Promise((resolve, reject) => {
+    const blogPost = path.resolve('./src/templates/post.jsx');
+    resolve(
+      graphql(
+        `
+          {
+            allMarkdownRemark(
+              sort: { fields: [frontmatter___date], order: DESC }
+              limit: 1000
+            ) {
+              edges {
+                node {
+                  fields {
+                    slug
+                  }
+                  frontmatter {
+                    title
+                  }
+                }
+              }
+            }
+          }
+        `,
+      ).then((result) => {
+        if (result.errors) {
+          reject(result.errors);
+        }
+
+        const posts = result.data.allMarkdownRemark.edges;
+
+        _.forEach(posts, ({ node }) => {
+          createPage({
+            path: node.fields.slug,
+            component: blogPost,
+            context: {
+              slug: node.fields.slug,
+            },
+          });
+        });
+      }),
+    );
+  });
+};
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === 'MarkdownRemark') {
+    const value = createFilePath({ node, getNode });
+    createNodeField({
+      name: 'slug',
+      node,
+      value,
+    });
+  }
+};
