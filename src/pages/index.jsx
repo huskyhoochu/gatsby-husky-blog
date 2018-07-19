f7568e4244ac9d1bda8859e29e89577ef8a2151f
huskyhoochu <dfg1499@gmail.com>
2018-07-19 15:25:39 +0900

    site metadata 를 graphql 로 파싱

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { graphql } from 'gatsby';
 
 // CSS
 import 'bulma/css/bulma.min.css';
@@ -6,10 +9,38 @@ import 'bulma/css/bulma.min.css';
 // Components
 import Layout from '../components/layout/Layout';
 
-export default () => (
+const BlogIndex = ({ data }) => (
   <div className="App">
+    <Helmet>
+      <title>{data.site.siteMetadata.title}</title>
+      <meta name="description" content={data.site.siteMetadata.description} />
+    </Helmet>
     <Layout>
       <h1 className="title">Welcome to huskyhoochu blog</h1>
     </Layout>
   </div>
 );
+
+BlogIndex.propTypes = {
+  data: PropTypes.shape({
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default BlogIndex;
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`;
