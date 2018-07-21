bd3475ab488ecfe71abf1cc43050b1cf64bc4339
huskyhoochu <dfg1499@gmail.com>
2018-07-21 19:09:51 +0900

    마크다운을 위한 이미지 프로세싱 플러그인, 참조 위치 보정 플러그인 설치

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Huskyhoochu',
     description: '허스키후추의 개발 블로그',
+    author: 'Husky',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -29,6 +30,25 @@ module.exports = {
         name: 'assets',
       },
     },
-    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 600,
+              wrapperStyle: {
+                margin: 'auto',
+                height: '100%',
+              },
+            },
+          },
+          'gatsby-remark-copy-linked-files',
+        ],
+      },
+    },
   ],
 };
