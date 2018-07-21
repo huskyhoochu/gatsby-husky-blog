7019e23580c064981a7d736148f16a76987b28eb
huskyhoochu <dfg1499@gmail.com>
2018-07-21 19:12:13 +0900

    메타데이터 작가 이름 불러오기

--- src/templates/BlogPost.jsx
+++ src/templates/BlogPost.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 
+// CSS
+import './BlogPost.css';
+
 // Component
 import Layout from '../components/layout/Layout';
 
 const BlogPost = ({ data }) => {
-  const { markdownRemark } = data;
+  const { markdownRemark, site } = data;
 
   return (
     <Layout>
       <h1>{markdownRemark.frontmatter.title}</h1>
       <h1>{markdownRemark.frontmatter.date}</h1>
+      <h1>{site.siteMetadata.author}</h1>
       <div
         id="content"
         dangerouslySetInnerHTML={{ __html: markdownRemark.html }}
@@ -22,6 +26,12 @@ const BlogPost = ({ data }) => {
 
 BlogPost.propTypes = {
   data: PropTypes.shape({
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        author: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired,
     markdownRemark: PropTypes.shape({
       frontmatter: PropTypes.shape({
         date: PropTypes.string,
@@ -36,6 +46,12 @@ export default BlogPost;
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
     markdownRemark(fields: { slug: { eq: $slug } }) {
       id
       html
