d3b88cc6be5a25cbea597a87fb6b566170cac902
huskyhoochu <dfg1499@gmail.com>
2018-07-23 19:13:14 +0900

    한글 사이트명 추가

--- src/data/SiteConfig.js
+++ src/data/SiteConfig.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteTitle: 'Huskyhoochu',
-  siteDescription: '허스키후추의 개발 블로그',
+  siteTitleKorean: '허스키후추의 개발 블로그',
+  siteDescription:
+    '허스키후추의 개발 스토리와 여러 끄적거림을 담아둔 공간입니다.',
   siteUrl: 'https://www.huskyhoochu.com',
   author: 'husky',
   googleVerificationCode: 'GgxdKaAIavUipoP0qqmrusNSygoGsdMVZgMtzowaZCo',
