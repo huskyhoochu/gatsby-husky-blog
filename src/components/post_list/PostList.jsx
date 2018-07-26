d4ad6f290d3dfce264cd825ba8853c88aef0b499
huskyhoochu <dfg1499@gmail.com>
2018-07-26 19:23:49 +0900

    postList를 감싸는 postWrapper 컴포넌트 생성

--- src/components/post_list/PostList.jsx
+++ src/components/post_list/PostList.jsx
@@ -1,17 +1,33 @@
 import _ from 'lodash';
-import React from 'react';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 
+// Styled
+import Styled from './StyledPostList';
+
 const PostList = ({ content }) => _.map(content, ({ node }) => {
-  const { title } = node.frontmatter;
+  const { date, title } = node.frontmatter;
   return (
       <div key={node.fields.slug}>
         <Link to={node.fields.slug}>
           <h3>{title}</h3>
+          <p>{date}</p>
           <p>{node.excerpt}</p>
         </Link>
       </div>
   );
 });
 
-export default PostList;
+const PostWrapper = ({ edges }) => (
+  <Fragment>
+    <Styled.CategoryTitle>hi</Styled.CategoryTitle>
+    <PostList content={edges} />
+  </Fragment>
+);
+
+PostWrapper.propTypes = {
+  edges: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default PostWrapper;
