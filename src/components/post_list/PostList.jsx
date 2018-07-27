acc2b3b84897ba085bd01c75785f57e69bc15895
huskyhoochu <dfg1499@gmail.com>
2018-07-27 17:18:46 +0900

    리스트 썸네일에 이벤트 리스너 설치

--- src/components/post_list/PostList.jsx
+++ src/components/post_list/PostList.jsx
@@ -6,32 +6,48 @@ import { Link } from 'gatsby';
 // Styled
 import Styled from './StyledPostList';
 
-const PostList = ({ content }) => {
-  const { markdown, imgSharp } = content;
+class PostList extends React.Component {
+  static handleTouch(e) {
+    e.currentTarget.classList.toggle('move');
+  }
 
-  return _.map(_.zip(markdown, imgSharp), (item) => {
-    const markdownItem = item[0].node;
-    const { date, title } = markdownItem.frontmatter;
+  componentDidMount() {
+    const el = document.querySelectorAll('.thumbItem');
+    _.forEach(el, (node) => {
+      node.addEventListener('touchstart', PostList.handleTouch, false);
+      node.addEventListener('mouseenter', PostList.handleTouch, false);
+      node.addEventListener('mouseleave', PostList.handleTouch, false);
+    });
+  }
 
-    const imgSharpItem = item[1].node;
-    const { publicURL, childImageSharp } = imgSharpItem;
+  render() {
+    const { content } = this.props;
+    const { markdown, imgSharp } = content;
 
-    return (
-      <Styled.PostBody key={markdownItem.fields.slug}>
-        <Link to={markdownItem.fields.slug}>
-          <img
-            src={publicURL}
-            srcSet={childImageSharp.fluid.srcSet}
-            alt="post-thumb"
-          />
-          <h3>{title}</h3>
-          <Styled.PostDate>{date}</Styled.PostDate>
-          <Styled.PostExcerpt>{markdownItem.excerpt}</Styled.PostExcerpt>
-        </Link>
-      </Styled.PostBody>
-    );
-  });
-};
+    return _.map(_.zip(markdown, imgSharp), (item) => {
+      const markdownItem = item[0].node;
+      const { date, title } = markdownItem.frontmatter;
+
+      const imgSharpItem = item[1].node;
+      const { publicURL, childImageSharp } = imgSharpItem;
+
+      return (
+        <Styled.PostBody key={markdownItem.fields.slug}>
+          <Link to={markdownItem.fields.slug}>
+            <Styled.PostThumb
+              src={publicURL}
+              srcSet={childImageSharp.fluid.srcSet}
+              alt="post-thumb"
+            />
+            <h3>{title}</h3>
+            <Styled.PostDate>{date}</Styled.PostDate>
+            <Styled.PostExcerpt>{markdownItem.excerpt}</Styled.PostExcerpt>
+          </Link>
+        </Styled.PostBody>
+      );
+    });
+  }
+}
 
 const PostWrapper = ({ edges }) => (
   <Fragment>
@@ -41,6 +57,13 @@ const PostWrapper = ({ edges }) => (
     </Styled.PostWrapper>
   </Fragment>
 );
+
+PostList.propTypes = {
+  content: PropTypes.shape({
+    markdown: PropTypes.array.isRequired,
+    imgSharp: PropTypes.array.isRequired,
+  }).isRequired,
+};
 
 PostWrapper.propTypes = {
   edges: PropTypes.shape({
