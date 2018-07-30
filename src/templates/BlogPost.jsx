947a12610753baf5ff778aba954110b40dc01120
huskyhoochu <dfg1499@gmail.com>
2018-07-30 16:27:48 +0900

    layout -> layouts

--- src/templates/BlogPost.jsx
+++ src/templates/BlogPost.jsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from 'styled-components';
 import Styled from './StyledBlogPost';
 
 // Component
-import Layout from '../layout/Layout';
+import Layout from '../layouts/Layout';
 import LeftSection from '../components/left_section/LeftSection';
 import RightSection from '../components/right_section/RightSection';
 import PostInformation from '../components/post_information/PostInformation';
