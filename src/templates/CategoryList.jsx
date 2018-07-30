a39e22bf5e6ae3295899a268461c724c99f2960e
huskyhoochu <dfg1499@gmail.com>
2018-07-30 20:43:20 +0900

    구조 설계

--- src/templates/CategoryList.jsx
+++ src/templates/CategoryList.jsx
@@ -1,21 +1,55 @@
 import _ from 'lodash';
 import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 import { graphql } from 'gatsby';
 
 // Components
-// import PostList from '../components/post_list/PostList';
+import Layout from '../layouts/Layout';
 
-const CategoryList = ({ data }) => {
-  const { allMarkdownRemark } = data;
-  return _.map(allMarkdownRemark.edges, ({ node }) => (
-    <p>{node.frontmatter.title}</p>
-  ));
+const CategoryList = ({ data, pathContext }) => {
+  const { site } = data;
+  const { category } = pathContext;
+
+  return (
+    <Layout>
+      <Helmet>
+        <link
+          rel="canonical"
+          href={`${site.siteMetadata.siteUrl}/categories/${_.kebabCase(
+            category,
+          )}`}
+        />
+        <title>{`Category as "${category}"| ${
+          site.siteMetadata.siteTitle
+        }`}</title>
+      </Helmet>
+    </Layout>
+  );
+};
+
+CategoryList.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.object.isRequired,
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.object.isRequired,
+    }).isRequired,
+  }).isRequired,
+  pathContext: PropTypes.shape({
+    category: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default CategoryList;
 
 export const pageQuery = graphql`
   query CategoryPage($category: String!) {
+    site {
+      siteMetadata {
+        siteTitle
+        siteUrl
+      }
+    }
     allMarkdownRemark(
       sort: { fields: [frontmatter___date], order: DESC }
       filter: { frontmatter: { category: { eq: $category } } }
