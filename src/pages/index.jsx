e5ee98806f4f57d10141e88cd6561500c58cd790
huskyhoochu <dfg1499@gmail.com>
2018-07-31 15:53:03 +0900

    디폴트 카테고리 'Latest' 전달

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,6 +24,7 @@ const BlogIndex = ({ data }) => {
         },
         markdownItem: {},
         thumbnail: file.childImageSharp.fluid.src,
+        category: 'Latest',
       }}
     >
       <Helmet>
