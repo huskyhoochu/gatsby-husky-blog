928d2384d51203e2a0f26f4fa861022c02046a63
huskyhoochu <dfg1499@gmail.com>
2018-08-01 21:36:05 +0900

    SEOHelmet 컴포넌트로 layout helmet 정보 옮김

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
-import config from '../data/SiteConfig';
 
 // Styled
 import Styled from './StyledLayout';
@@ -12,78 +10,17 @@ import SideMenu from '../components/side_menu/SideMenu';
 import GreyOut from '../components/grey_out/GreyOut';
 import FlexWrapper from '../components/flex_wrapper/FlexWrapper';
 
-const Layout = ({ children, query }) => {
-  const schemaOrgJSON = [
-    {
-      '@context': 'http://schema.org',
-      '@type': 'WebSite',
-      url: config.siteUrl,
-      name: config.siteTitle,
-    },
-  ];
-
-  return (
-    <Styled.App>
-      <Helmet>
-        <html lang="ko" />
-        <meta
-          name="google-site-verification"
-          content={config.googleVerificationCode}
-        />
-        <meta name="author" content={config.author} />
-        <script type="application/ld+json">
-          {JSON.stringify(schemaOrgJSON)}
-        </script>
-        <script async src={config.googleAdSenseEndpoint} />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon_package/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon_package/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/favicon_package/android-chrome-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon_package/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon_package/site.webmanifest.json" />
-        <link
-          rel="mask-icon"
-          href="/favicon_package/safari-pinned-tab.svg"
-          color="#663399"
-        />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta
-          name="msapplication-TileImage"
-          content="/favicon_package/mstile-144x144.png"
-        />
-        <meta name="theme-color" content="#ffffff" />
-        <meta property="og:image" content="/favicon_package/og-image.jpg" />
-        <meta property="og:image:width" content="279" />
-        <meta property="og:image:height" content="279" />
-      </Helmet>
-      <Styled.Canvas>
-        <SideMenu />
-        <Header />
-        <FlexWrapper query={query} />
-        {children}
-        <GreyOut />
-      </Styled.Canvas>
-    </Styled.App>
-  );
-};
+const Layout = ({ children, query }) => (
+  <Styled.App>
+    <Styled.Canvas>
+      <SideMenu />
+      <Header />
+      <FlexWrapper query={query} />
+      {children}
+      <GreyOut />
+    </Styled.Canvas>
+  </Styled.App>
+);
 
 Layout.propTypes = {
   children: PropTypes.element.isRequired,
