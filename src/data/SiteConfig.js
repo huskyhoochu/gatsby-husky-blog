4a0a5d3ff092b12bcc2c7854095636f11b7d8bee
huskyhoochu <dfg1499@gmail.com>
2018-08-03 18:19:16 +0900

    facebook ID 추가

--- src/data/SiteConfig.js
+++ src/data/SiteConfig.js
@@ -5,6 +5,7 @@ module.exports = {
     '허스키후추의 개발 스토리와 여러 끄적거림을 담아둔 공간입니다.',
   siteUrl: 'https://www.huskyhoochu.com',
   author: 'husky',
+  facebookAppId: '276928816428659',
   google: {
     verificationCode: 'GgxdKaAIavUipoP0qqmrusNSygoGsdMVZgMtzowaZCo',
     analyticsId: 'UA-100360993-4',
