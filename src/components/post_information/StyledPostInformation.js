b1a5976218c08592389f5bf459b865e927d30c49
huskyhoochu <dfg1499@gmail.com>
2018-08-03 18:21:29 +0900

    공유 버튼을 위한 Styled 컴포넌트 생성

---
+++ src/components/post_information/StyledPostInformation.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const IconButton = styled.button.attrs({
+  type: 'button',
+})`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  margin: 0;
+  padding: 0;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Icon = styled.img.attrs({
+  alt: 'share-button',
+})`
+  width: 1.5rem;
+  height: 1.5rem;
+`;
+
+export default {
+  IconButton,
+  Icon,
+};
