865fbbd12bfa594aa13eba4dfb77b79d5b06fe83
huskyhoochu <dfg1499@gmail.com>
2018-08-08 16:56:45 +0900

    목록 돌아가기 버튼 생성

--- src/components/flex_wrapper/FlexWrapper.jsx
+++ src/components/flex_wrapper/FlexWrapper.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
 import AdSense from 'react-adsense';
 import { ThemeProvider } from 'styled-components';
 import config from '../../data/SiteConfig';
@@ -40,6 +41,23 @@ const FlexWrapper = ({ query }) => {
           <Styled.Content
             dangerouslySetInnerHTML={{ __html: markdownItem.html }}
           />
+          <Styled.Content>
+            <hr />
+          </Styled.Content>
+          <Styled.Content style={{ textAlign: 'center' }}>
+            {category !== undefined ? (
+              <Link
+                to={`/categories/${category}`}
+                style={{ color: 'rebeccapurple' }}
+              >
+                <h5>List</h5>
+              </Link>
+            ) : (
+              <Link to="/" style={{ color: 'rebeccapurple' }}>
+                <h5>Home</h5>
+              </Link>
+            )}
+          </Styled.Content>
           <Styled.Content>
             <AdSense.Google
               client={config.google.adSense.clientId}
