988074018208c7d62d6b75f345126e78ab5a9409
huskyhoochu <dfg1499@gmail.com>
2018-08-15 16:45:30 +0900

    [#6] [hot fix] 'document is not defined' 에러 수정
    - if typeof ... 조건문으로 에러 회피

--- src/components/post_detail/PostDetail.jsx
+++ src/components/post_detail/PostDetail.jsx
@@ -9,25 +9,27 @@ import config from '../../data/SiteConfig';
 import Styled from '../flex_wrapper/StyledFlexWrapper';
 
 const wrapTableElement = () => {
-  const nodeList = document.querySelectorAll('table');
+  if (document.querySelector('window')) {
+    const nodeList = document.querySelectorAll('table');
 
-  _.forEach(nodeList, (node) => {
-    const newDiv = document.createElement('div');
-    newDiv.className = 'table-wrapper';
-    node.parentNode.insertBefore(newDiv, node);
-  });
+    _.forEach(nodeList, (node) => {
+      const newDiv = document.createElement('div');
+      newDiv.className = 'table-wrapper';
+      node.parentNode.insertBefore(newDiv, node);
+    });
 
-  const wrapperList = document.querySelectorAll('.table-wrapper');
+    const wrapperList = document.querySelectorAll('.table-wrapper');
 
-  _.forEach(_.zip(nodeList, wrapperList), (item) => {
-    item[1].appendChild(item[0]);
-  });
+    _.forEach(_.zip(nodeList, wrapperList), (item) => {
+      item[1].appendChild(item[0]);
+    });
+  }
 };
 
 const PostDetail = ({ edges }) => {
   const { markdownItem, category } = edges;
 
-  if (document.querySelector('table')) {
+  if (typeof window !== 'undefined') {
     wrapTableElement();
   }
 
