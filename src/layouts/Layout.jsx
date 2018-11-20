9597da4acc94ac8e74ba04e9809b4fe7779f4266
huskyhoochu <dfg1499@gmail.com>
2018-11-20 19:43:02 +0900

    [#32] webfontloader 위치 이동
    - index.jsx 에서 layout.jsx로 이동

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../utils/WebFontLoader';
 
 // Styled
 import Styled from './StyledLayout';
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
-import '../utils/WebFontLoader';
 
 // Components
 import Layout from '../layouts/Layout';
